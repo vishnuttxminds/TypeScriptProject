7b085ab8518e80fac981ef35f79a8240ba2f44c8
vishnutt <vishnutt@xminds.com>
2024-03-08 17:40:23 +0530

    type script examples

---
+++ build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let username = "Thoma";
+console.log(username);
+let q = 1;
+let b = 3;
+let c = 10;
+console.log(q + c);
---
+++ src/main.ts
@@ -0,0 +1,38 @@
+let username = "Thoma";
+console.log(username);
+
+let q = 1;
+let b = 3;
+let c = 10;
+console.log(q + c);
+
+let registerNumber: string | number;
+
+registerNumber = "constrdsc57dhak";
+registerNumber = 3;
+
+let stringArrNumber = ["one", "two", 3];
+let stringArrFruits = ["Apple", "Orange", "Grape", "kivi"];
+
+stringArrNumber[0] = "five";
+stringArrNumber.push("four");
+
+stringArrFruits.unshift("Jim");
+
+let test = [];
+let band: string[] = [];
+band.push("Helo");
+
+let mutipleArray: [string, number, boolean] = ["Thoms", 980, true];
+
+
+
+console.log("====================================");
+console.log(stringArrNumber);
+
+console.log(stringArrFruits);
+
+console.log(band);
+
+console.log(mutipleArray);
+console.log("====================================");
