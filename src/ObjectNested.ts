d93e1ba1ba10bf3cf6a00bce71c46cb264bde0ba
vishnutt <vishnutt@xminds.com>
2024-03-12 18:28:55 +0530

    nested object updated

--- src/ObjectNested.ts
+++ src/ObjectNested.ts
@@ -1,29 +1,30 @@
 const person = {
-    Name: "Johny",
-    Age: 40,
-    Cars: [
-      {
-        name: "ford",
-        models: ["mustang", "Fiesta", "Mustang"]
-      },
-      {
-        name: "BMW",
-        models: [320, "x3", "x5"]
-      },
-      {
-        name: "fiat",
-        models: [500, "panda"]
-      }
-    ]
-  };
+  Name: "Johny",
+  Age: 40,
+  Cars: [
+    {
+      name: "ford",
+      models: ["mustang", "Fiesta", "Mustang"],
+    },
+    {
+      name: "BMW",
+      models: [320, "x3", "x5"],
+    },
+    {
+      name: "fiat",
+      models: [500, "panda"],
+    },
+  ],
+};
 
+console.log("Name > ", person.Name);
+console.log("Age > ", person.Age);
 
-  console.log('Cars > ' , person.Cars);
+console.log("Cars > ", person.Cars);
 
-  person.Cars.forEach(element => {
-    console.log('Models > ', element.models);
+person.Cars.forEach((element) => {
+  console.log("Cars Name > ", element.name);
+  element.models.forEach((models) => {
+    console.log("Cars Model Name > ", models);
   });
-
-
-  
-  
+});
