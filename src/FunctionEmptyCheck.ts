8bf598dd2b45315839277efd52650f42ed88e3b2
vishnutt <vishnutt@xminds.com>
2024-03-12 13:21:16 +0530

    object empty check or not

---
+++ src/FunctionEmptyCheck.ts
@@ -0,0 +1,17 @@
+let objPerson = {
+  name: "John",
+  age: 23,
+  degree: "CS",
+};
+
+let objEmpty = {};
+
+const checkFunctionData = (obj: Object) => {
+  if (Object.keys(obj).length == 0) {
+    return true;
+  }
+  return false;
+};
+
+console.log("Object Empty is", checkFunctionData(objPerson));
+console.log("Object Empty is", checkFunctionData(objEmpty));
