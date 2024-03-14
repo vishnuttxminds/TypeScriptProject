be13328ac55bfa8c8e3bf0313898b9c05e71a6dd
vishnutt <vishnutt@xminds.com>
2024-03-14 16:31:20 +0530

    update display values

--- src/Assignments/DisplyValueObjDescrt.ts
+++ src/Assignments/DisplyValueObjDescrt.ts
@@ -1,3 +1,6 @@
+
+// 5.Display values of the following object with object destructuring.
+
 const item = {
   nameItem: "shoes",
   size: {
@@ -14,8 +17,8 @@ const {
   size: { EU, US },
 } = item;
 
-console.log('Item Name : ' + nameItem);
-console.log('Item size : ' + JSON.stringify(size));
-console.log('Item statusItem : ' + statusItem);
-console.log('size EU : ' + EU);
-console.log('size US : ' + US);
+console.log('Item Name : ' , nameItem);
+console.log('Item size : ' , size);
+console.log('Item statusItem : ' , statusItem);
+console.log('size EU : ' , EU);
+console.log('size US : ' , US);
