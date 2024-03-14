686e659918b5ad2f1694511d0a72a9caf9095844
vishnutt <vishnutt@xminds.com>
2024-03-14 18:39:51 +0530

    currency convertion

---
+++ src/Assignments/CurrencyConvert.ts
@@ -0,0 +1,34 @@
+// 6.Define a convert currency function that takes an amount and returns an object that has U.S.
+// dollars, British pounds, Australian dollars, and Mexican pesos using
+// destructuring display each currency value individually.
+
+const currencyInfo = [
+  {
+    counrty: "MEX",
+    currency: 43,
+  },
+  {
+    counrty: "AUS",
+    currency: 44,
+  },
+  {
+    counrty: "US",
+    currency: 45,
+  },
+  {
+    counrty: "ENG",
+    currency: 46,
+  },
+];
+
+const convertCurrency = (
+  amount: number,
+  counrty: string,
+  currency: number
+) => {
+  return `Indian rupee amount ${amount} in ${counrty} is ${amount * currency}`;
+};
+
+currencyInfo.forEach((element) => {
+  console.log(convertCurrency(106, element.counrty, element.currency));
+});
