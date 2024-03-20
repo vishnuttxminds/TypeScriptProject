d7fc3a62e9161216719b92b3cee9f6ec5be6d651
vishnutt <vishnutt@xminds.com>
2024-03-20 16:08:14 +0530

    updation in GuessingGame

--- src/Assignments/GuessingGame.ts
+++ src/Assignments/GuessingGame.ts
@@ -9,38 +9,46 @@ async function getRandomNumber(max: number) {
 async function pointCaluculations(userInput: number) {
   const randomNumber = await getRandomNumber(6);
 
-  if (userInput > 0) {
-    if (userInput === randomNumber) {
-      return 2;
-    } else if (
-      userInput - randomNumber === 1 ||
-      userInput - randomNumber === -1
-    ) {
-      return 1;
+  const calculationPromise = new Promise(async (resolve, reject) => {
+    if (userInput > 0) {
+      if (userInput === randomNumber) {
+        resolve(2);
+      } else if (
+        userInput - randomNumber === 1 ||
+        userInput - randomNumber === -1
+      ) {
+        resolve(1);
+      } else {
+        resolve(0);
+      }
     } else {
-      return 0;
+      resolve(3);
     }
-  } else {
-    return 3;
-  }
+  });
+
+  return calculationPromise;
 }
 
 async function enterValue() {
   const userInput = prompt("Enter the number : ");
-  const promiseGame = new Promise(async (resolve, reject) => {
-    const response = await pointCaluculations(parseInt(userInput));
-
-    if (response === 2) {
-      resolve("You win 2 pints !!");
-    } else if (response === 1) {
-      resolve("You win 1 pints !!");
-    } else if (response === 0) {
-      resolve("You win 0 pints !!");
-    } else {
-      reject();
-    }
+  const enterPromise = new Promise(async (resolve, reject) => {
+    pointCaluculations(parseInt(userInput))
+      .then((response) => {
+        if (response === 2) {
+          resolve("You win 2 pints !!");
+        } else if (response === 1) {
+          resolve("You win 1 pints !!");
+        } else if (response === 0) {
+          resolve("You win 0 pints !!");
+        } else {
+          reject();
+        }
+      })
+      .catch((error) => {
+        reject();
+      });
   });
-  return promiseGame;
+  return enterPromise;
 }
 function countinue(yesOrNo: string) {
   if (yesOrNo === "") {
@@ -60,6 +68,7 @@ function countinue(yesOrNo: string) {
     console.log("Finished , Thanks !!");
   }
 }
+
 countinue("Y");
 
 // enterValue()
