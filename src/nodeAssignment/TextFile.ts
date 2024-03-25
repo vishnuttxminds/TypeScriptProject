8b2fe7558c90dd2e2b5d2597e5b9ac3470a7d4bd
vishnutt <vishnutt@xminds.com>
2024-03-25 16:03:14 +0530

    changes

--- src/nodeAssignment/TextFile.ts
+++ src/nodeAssignment/TextFile.ts
@@ -3,16 +3,10 @@ import * as fs from 'fs';
 
 
 const server = http.createServer((request, response) => {
-    // Set the content type to text/plain
     response.setHeader('Content-Type', 'text/plain');
-
-    // Read the content of the text file using a readable stream
     const readStream = fs.createReadStream('Text.txt');
-
-    // Pipe the content of the file to the response stream
     readStream.pipe(response);
 
-    // Handle errors
     readStream.on('error', (err) => {
         console.error(`Error reading file: ${err}`);
         response.statusCode = 500;
@@ -22,7 +16,9 @@ const server = http.createServer((request, response) => {
 
 
 server.listen(3000, () => {
-    console.log(`Server is running on http://localhost:`);
+    console.log(`Server is running `);
 });
 
-console.log(server);
+
+
+
